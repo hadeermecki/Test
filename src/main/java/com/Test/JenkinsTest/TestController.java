f4f39c009c8af7f4eea258c3eb59eb233263de40
Hadeer Magdy Mahgoub <hmagdy@citglobal.com>
2019-04-13 23:39:39 +0200

    commit

---
+++ src/main/java/com/Test/JenkinsTest/TestController.java
@@ -0,0 +1,14 @@
+package com.Test.JenkinsTest;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class TestController {
+    @RequestMapping(value = "/get-welcome" , method = RequestMethod.GET)
+    public String Welcome (){
+        System.out.println("Welcomeeeeeee");
+        return "welcome";
+    }
+}
